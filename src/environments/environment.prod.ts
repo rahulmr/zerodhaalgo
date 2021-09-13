356e6ada27cce9fd110c8f5436c8fa8309a51797
gmanojisaac <gmanoj.isaac@gmail.com>
2021-09-13 10:31:16 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBivW__mV76H4nn_b95CecBCPgQLYdEaAQ",
-    authDomain: "zerodha-a1fc5.firebaseapp.com",
-    projectId: "zerodha-a1fc5",
-    storageBucket: "zerodha-a1fc5.appspot.com",
-    messagingSenderId: "592965746249",
-    appId: "1:592965746249:web:2c4b9241280caa14d88998",
-    measurementId: "G-2K9S3M0SDM"
+
   }  
 };
